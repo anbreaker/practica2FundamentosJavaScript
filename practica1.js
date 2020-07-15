5c69f982cff3abe44e0e17e8c428bbe01fe13222
anbreaker <antunez19@gmail.com>
2020-07-15 14:58:31 +0200

    iniciando practica

---
+++ practica1.js
@@ -0,0 +1,28 @@
+// Conversor de números romanos javascript
+// I         1             |   V       5             |   X        10
+// L         50           |   C        100         |   D       500 | M                1000
+
+valueNumRoman = {
+  1: 'I',
+  5: 'V',
+  9: 'IX',
+  10: 'X',
+  50: 'L',
+  99: 'XCIX',
+  100: 'C',
+  500: 'D',
+  999: 'CMXCIX',
+  1000: 'M',
+};
+
+valores = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1};
+valores5 = {D: 500, L: 50, V: 5};
+valores9 = {IX: 9, XCIX: 99, CMXCIX: 999};
+
+simbolosOrdenados = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+
+// Num Roman to Arabic
+
+function arabicToRoman(numArabic = 1) {
+  // Empezar por aqui...
+}
