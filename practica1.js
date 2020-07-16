6d7c0bdf24141c48b32f4391538b7aa63e2ffc10
anbreaker <antunez19@gmail.com>
2020-07-16 14:59:02 +0200

    observando funcionamiento clave valor js

--- practica1.js
+++ practica1.js
@@ -2,27 +2,36 @@
 // I         1             |   V       5             |   X        10
 // L         50           |   C        100         |   D       500 | M                1000
 
-valueNumRoman = {
-  1: 'I',
-  5: 'V',
-  9: 'IX',
-  10: 'X',
-  50: 'L',
-  99: 'XCIX',
-  100: 'C',
-  500: 'D',
-  999: 'CMXCIX',
-  1000: 'M',
+const dictRomans = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
 };
-
-valores = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1};
-valores5 = {D: 500, L: 50, V: 5};
-valores9 = {IX: 9, XCIX: 99, CMXCIX: 999};
-
-simbolosOrdenados = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
 
 // Num Roman to Arabic
 
-function arabicToRoman(numArabic = 1) {
-  // Empezar por aqui...
+function iterateDict() {
+  for (const key in dictRomans) {
+    console.log(`Clave -> ${key} \t Valor ${dictRomans[key]}`);
+  }
 }
+
+function arabicToRoman(numArabic) {
+  let numRoman = '';
+
+  for (let clave in dictRomans) {
+    console.log(clave);
+  }
+}
+
+arabicToRoman(17);
