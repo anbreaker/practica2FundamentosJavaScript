2607fc2ac7f1be773e46e782f69f26225844aa4a
anbreaker <antunez19@gmail.com>
2020-07-19 12:02:06 +0200

    anidando condiciones... argh

--- practica1.js
+++ practica1.js
@@ -60,7 +60,6 @@ function decomposingRomanNumber(romanNum) {
   let compare = romanNum[flag];
   while (flag < romanNum.length) {
     // Conditions order of roman letters
-    // CM, CD, XC, XL, IX, IV;
     compare = romanNum[flag];
 
     if (compare === romanNum[flag - 1]) {
@@ -71,18 +70,60 @@ function decomposingRomanNumber(romanNum) {
     }
 
     if (repetitions > 0) {
+      // CM, CD, XC, XL, IX, IV;
+
+      if (
+        (romanNum[flag] === 'V' && romanNum[flag - 1] === 'V') ||
+        (romanNum[flag] === 'L' && romanNum[flag - 1] === 'L') ||
+        (romanNum[flag] === 'L' && romanNum[flag - 1] === 'L') ||
+        (romanNum[flag] === 'C' && romanNum[flag - 1] === 'I')
+      ) {
+        return `\t--Only Roman numbers correct-- 
+        \t\tChange this number: ${romanNum.join('')}`;
+      } else if (
+        (romanNum[flag] === 'M' && romanNum[flag - 1]) === 'C' ||
+        (romanNum[flag] === 'D' && romanNum[flag - 1]) === 'C'
+      ) {
+        // console.log('here', romanNum[flag], romanNum[flag - 1]);
+        res -= 200;
+      } else if (
+        (romanNum[flag] === 'C' && romanNum[flag - 1]) === 'X' ||
+        (romanNum[flag] === 'L' && romanNum[flag - 1]) === 'X'
+      ) {
+        res -= 20;
+      } else if (
+        (romanNum[flag] === 'X' && romanNum[flag - 1]) === 'I' ||
+        (romanNum[flag] === 'V' && romanNum[flag - 1]) === 'I'
+      ) {
+        res -= 2;
+      }
       res += numRomans[romanNum[flag]];
     } else {
-      return '    --Only Roman numbers correct--   ';
+      return `\t--Only Roman numbers correct-- 
+      \t\tChange this number: ${romanNum.join('')}`;
     }
-
     flag += 1;
   }
   return res;
 }
 
-console.log(decomposingRomanNumber('MMMCCXL'));
-// console.log(decomposingRomanNumber('CDLIV'));
+function validateRomanNumber(romanNum) {
+  /* 
+    Sólo se contemplan números entre el 1 y el 3999
+      - Los símbolos I, X, C y M se pueden repetir hasta tres veces.
+      - Los símbolos V, L y D no pueden repetirse.
+      - Los símbolos I, X y C se suman si están a la derecha de otro mayor o igual
+      - Los símbolos I, X y C se restan si están a la izquierda de otro mayor y
+        solamente pueden anteponerse a los dos símbolos que les siguen en la sucesión
+      - I se resta de V y X
+      - X se resta de L y C
+      - C se resta de D y M
+      - Los símbolos V, L y D no pueden colocarse a la izquierda de otro mayor.
+  */
+}
+
+console.log(decomposingRomanNumber('IC'));
+console.log(decomposingRomanNumber('CDLIV'));
 
 /* Pruebas
 'LXXIII' --> (73)
