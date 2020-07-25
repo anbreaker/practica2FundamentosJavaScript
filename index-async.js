ccb8193199d824bb1c9664f418293245ae33c896
anbreaker <antunez19@gmail.com>
2020-07-25 19:33:38 +0200

    asincronia terminada!

--- index-async.js
+++ index-async.js
@@ -10,22 +10,30 @@ readFile('dataRandom.csv', 'utf-8', (err, data) => {
   } else {
     //Llamada a la funcion.
     console.log('Hola');
+
+    const arrayRomanNumber = data.split(/\r?\n/).map(function (arabNum) {
+      let transform = parseInt(arabNum);
+      if (!Number.isNaN(transform)) {
+        const res = decomposingArabNumber(arabNum);
+        console.log(`Arabic Number -> ${arabNum} \t Roman Number -> ${res}`);
+        return res;
+      } else {
+        const res = validateRomanNum(arabNum.toUpperCase());
+        console.log(`Roman Number -> ${res} \t Arabic Number -> ${arabNum}`);
+        return res;
+      }
+    });
+
+    // Observe the OS
+
+    writeFile(
+      'results-async.csv',
+      arrayRomanNumber.toString().replace(/,/g, '\n'),
+      function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      }
+    );
   }
 });
 console.log('Data Read Successfully!');
-
-const arrayRomanNumber = data.split(/\r?\n/).map(function (arabNum) {
-  let transform = parseInt(arabNum);
-  if (!Number.isNaN(transform)) {
-    const res = decomposingArabNumber(arabNum);
-    console.log(`Arabic Number -> ${arabNum} \t Roman Number -> ${res}`);
-    return res;
-  } else {
-    const res = validateRomanNum(arabNum.toUpperCase());
-    console.log(`Roman Number -> ${res} \t Arabic Number -> ${arabNum}`);
-    return res;
-  }
-});
-
-// Observe the OS
-writeFile('results-async.csv', arrayRomanNumber.toString().replace(/,/g, '\n'), 'utf-8');
